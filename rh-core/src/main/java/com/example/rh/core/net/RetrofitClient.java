16735595a83cca695f16b21a91db13d61654935f
RH <renhao1012@yeah.net>
2018-08-27 13:57:56 +0800

    创建Retrofit Service
     @Streaming 边下边写，避免一次性将内容下载到内存造成内存溢出。写入操作要另起线程。

---
+++ rh-core/src/main/java/com/example/rh/core/net/RetrofitClient.java
@@ -0,0 +1,8 @@
+package com.example.rh.core.net;
+
+/**
+ * @author RH
+ * @date 2018/8/27
+ */
+public class RetrofitClient {
+}
---
+++ rh-core/src/main/java/com/example/rh/core/net/RetrofitService.java
@@ -0,0 +1,46 @@
+package com.example.rh.core.net;
+
+import java.util.Map;
+
+import okhttp3.MultipartBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.DELETE;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Multipart;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Part;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+/**
+ * @author RH
+ * @date 2018/8/27
+ */
+public interface RetrofitService {
+    @GET
+    Call<String> get(@Url String url, @QueryMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @POST
+    Call<String> post(@Url String url, @FieldMap Map<String, Object> params);
+
+    @FormUrlEncoded
+    @PUT
+    Call<String> put(@Url String url, @FieldMap Map<String, Object> params);
+
+    @DELETE
+    Call<String> delete(@Url String url, @QueryMap Map<String, Object> params);
+
+    @Streaming
+    @GET
+    Call<ResponseBody> download(@Url String url, @QueryMap Map<String, Object> params);
+
+    @Multipart
+    @POST
+    Call<String> upload(@Url String url, @Part MultipartBody.Part file);
+}
